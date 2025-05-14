40b8ecbdff9414620b027b5e6d5fb39d2dce76d9
juktimath003 <juktiamthshreya123@gmail.com>
2025-05-14 11:46:35 +0530

    save now

---
+++ sub.java
@@ -0,0 +1,8 @@
+package pgm3;
+public class sub {
+    public static void main(String[] args) {
+        int num1 = 50, num2 = 20;
+        int result = num1 - num2;
+        System.out.println("The subtraction of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
